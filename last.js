1764fa052047a499168c8e03db798d3bf76fabd9
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2024-07-22 16:53:02 +0100

    Update last.js

--- last.js
+++ last.js
@@ -1,18 +1,1 @@
-    // Fetch the geo data from the API
-    fetch('https://get.geojs.io/v1/ip/geo.json')
-        .then(response => response.json())
-        .then(apiResponse => {
-            // Define the list of allowed country codes
-            const allowedCountryCodes = ["NE", "IR", "CD", "BR", "ET", "TZ", "ZA", "KE", "SD", "BD", "ID", "MA", "DZ"];
-
-            // Check if the country code exists in the list
-            if (allowedCountryCodes.includes(apiResponse.country_code)) {
-                // Modify the href attributes of the links
-                document.querySelectorAll('.match-container a').forEach(link => {
-                    link.href = "https://korashot.com";
-                });
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching geo data:', error);
-        });
+Util.findPreviousElementSibling=function(i){if(void 0!==i.previousElementSibling)return i.previousElementSibling;for(var e=i.previousSibling;e&&1!==e.nodeType;)e=e.previousSibling;return e};Util.findPreviousElementSibling=function(i){if(void 0!==i.previousElementSibling)return i.previousElementSibling;for(var e=i.previousSibling;e&&1!==e.nodeType;)e=e.previousSibling;return e};Util.findPreviousElementSibling=function(i){if(void 0!==i.previousElementSibling)return i.previousElementSibling;for(var e=i.previousSibling;e&&1!==e.nodeType;)e=e.previousSibling;return e};Util.findPreviousElementSibling=function(i){if(void 0!==i.previousElementSibling)return i.previousElementSibling;for(var e=i.previousSibling;e&&1!==e.nodeType;)e=e.previousSibling;return e};Util.findPreviousElementSibling=function(i){if(void 0!==i.previousElementSibling)return i.previousElementSibling;for(var e=i.previousSibling;e&&1!==e.nodeType;)e=e.previousSibling;return e};
