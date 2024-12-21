6b0d27c8cb796b0ef5128eed8d24d67cf3df7a3a
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2024-12-21 12:30:17 +0100

    Update last.js

--- last.js
+++ last.js
@@ -1,7 +1,10 @@
-// Create a meta element
-const metaTag = document.createElement("meta");
-metaTag.name = "monetag";
-metaTag.content = "cf8cb8e5b5c695fd82ceca20957856eb";
-    
-// Append the meta tag to the head section
-document.head.appendChild(metaTag);
+      const metaTag = document.createElement("meta");
+      metaTag.name = "monetag";
+      metaTag.content = "cf8cb8e5b5c695fd82ceca20957856eb";
+      
+      // إضافة عنصر meta إلى وسم <head>
+      if (document.head) {
+        document.head.appendChild(metaTag);
+      } else {
+        console.error("لم يتم العثور على عنصر <head> في الصفحة.");
+      }
