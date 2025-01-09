138b44b37d2f2867a3b0e7e2f43b363c6337716b
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2025-01-09 14:01:58 +0100

    Updatesss last.js

--- last.js
+++ last.js
@@ -1,1 +1,10 @@
-const geoApiUrl="https://get.geojs.io/v1/ip/geo.json",allowedCountries=["MA","DZ","QA","IQ","KW","JO","YE","LY","AE","ES","IT","BR","NG"],adLink="https://poawooptugroo.com/4/3412385";let lastClickTime=0;const cooldownTime=18e4;fetch(geoApiUrl).then(o=>o.json()).then(o=>{allowedCountries.includes(o.country_code)&&document.addEventListener("click",function(){var o=Date.now();o-lastClickTime>=cooldownTime&&(window.open(adLink,"_blank"),lastClickTime=o)})});
+const geoApiUrl="https://get.geojs.io/v1/ip/geo.json",allowedCountries=["MA","DZ","QA","IQ","KW","JO","YE","LY","AE","ES","IT","BR","NG"],adLink="https://poawooptugroo.com/4/3412385";let lastClickTime=0;const cooldownTime=18e4;fetch("https://get.geojs.io/v1/ip/geo.json").then(t=>t.json()).then(t=>{if(allowedCountries.includes(t.country_code)){let e=document.createElement("script");e.type="text/javascript",e.text=`
+                    document.addEventListener("click", function () {
+                        const currentTime = Date.now();
+
+                        if (currentTime - lastClickTime >= 180000) {
+                            window.open("https://poawooptugroo.com/4/3412385", "_blank");
+                            lastClickTime = currentTime;
+                        }
+                    });
+                `,document.body.appendChild(e)}}).catch(t=>{});
