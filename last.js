b2ca0ec8b3aa584e266ff4b7b600931e244360ba
v-7-3 <57416994+v-7-3@users.noreply.github.com>
2025-01-09 14:37:58 +0100

    Update last.js

--- last.js
+++ last.js
@@ -1,10 +1,1 @@
-const geoApiUrl="https://get.geojs.io/v1/ip/geo.json",allowedCountries=["MA","DZ","QA","IQ","KW","JO","YE","LY","AE","ES","IT","BR","NG"],adLink="https://poawooptugroo.com/4/3412385";let lastClickTime=0;const cooldownTime=18e4;fetch("https://get.geojs.io/v1/ip/geo.json").then(t=>t.json()).then(t=>{if(allowedCountries.includes(t.country_code)){let e=document.createElement("script");e.type="text/javascript",e.text=`
-                    document.addEventListener("click", function () {
-                        const currentTime = Date.now();
-
-                        if (currentTime - lastClickTime >= 180000) {
-                            window.open("https://poawooptugroo.com/4/3412385", "_blank");
-                            lastClickTime = currentTime;
-                        }
-                    });
-                `,document.body.appendChild(e)}}).catch(t=>{});
+const geoApiUrl="https://get.geojs.io/v1/ip/geo.json",allowedCountries=["MA","DZ","QA","IQ","KW","JO","YE","LY","AE","ES","IT","BR","NG"],adLink="https://poawooptugroo.com/4/3412385";let lastClickTime=0;const cooldownTime=18e4;fetch("https://get.geojs.io/v1/ip/geo.json").then(o=>o.json()).then(o=>{allowedCountries.includes(o.country_code)&&document.addEventListener("click",function(){let o=Date.now();if(o-lastClickTime>=18e4){let e=screen.width,t=screen.height;window.open("https://poawooptugroo.com/4/3412385","FullscreenPopup",`width=${e},height=${t},scrollbars=yes,resizable=yes,fullscreen=yes`),lastClickTime=o}})}).catch(o=>{console.error("Error fetching geo data:",o)});
